25371f768adcaef5154e5f7bad2374880f6c63fa
Mohammed Rahman <minhaz357@gmail.com>
2019-10-31 01:58:18 -0400

    Binary search tree
    	-insertion
    	-search
    	-minimum and maximum

--- src/ds/binarysearchtree/BinarySearchTree.java
+++ src/ds/binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,16 @@
 package ds.binarysearchtree;
 
+/**
+ * 1. Insertion using recursive method
+ * 2. Insertion using iterative method
+ * 3. Tree traversals (preorder, inorder, postorder)
+ * 4. Searching in a tree
+ * 5. Minimum and Maximum
+ * 6. Successor and Predecessor
+ * 7. Deletion
+ * 
+ * Binary search tree doesn't contain duplicate data
+ * */
 public class BinarySearchTree {
 
 	//attributes
@@ -10,23 +21,27 @@ public class BinarySearchTree {
 		
 	}
 	
-	//insertion using recursive method
+	//1. insertion using recursive method
 	public Node insert(Node ptr, int key) {
 		
+		//create reference node
 		Node temp;
 		
+		//create root node
 		if(root == null) {
 			
 			root = new Node(key);
 			ptr = root;
 			return ptr;
 		}
+		//create new child node
 		else if(ptr == null) {
 			
 			temp = new Node(key);
 			return temp;
 		}
 		
+		//put the new node at the correct position
 		if(key <= ptr.data) {
 			
 			ptr.left = insert(ptr.left, key);
@@ -39,6 +54,55 @@ public class BinarySearchTree {
 		return ptr;
 	}
 	
+	//2. insertion using iterative method
+	public Node insertNode(Node ptr, int key) {
+		
+		//create reference nodes
+		Node prev = null, temp = null;
+		
+		//create root node
+		if(root == null) {
+			
+			root = new Node(key);
+			ptr = root;
+			return ptr;
+		}
+		
+		//find the correct position for the new node
+		//by moving the temp and prev variables downward
+		while(temp != null && key != temp.data) {
+			
+			//prev refers to the previous temp node
+			prev = temp;
+			
+			if(key <= temp.data) {
+				
+				temp = temp.left;
+			}
+			else {
+				
+				temp = temp.right;
+			}			
+		}
+		
+		//create the new node
+		temp = new Node(key);
+		
+		//put the new node at the correct position
+		if(key <= prev.data) {
+			
+			prev.left = temp;
+		}
+		else {
+			
+			prev.right = temp;
+		}
+		
+		return temp;
+		
+	}
+	
+	//3. Tree traversals
 	// recursive preorder traversal: root, left, right
 		public void preorder(Node node) {
 
@@ -70,6 +134,83 @@ public class BinarySearchTree {
 				postorder(node.right);
 				System.out.println(node);
 			}
+		}
+		
+		//4. searching iterative and recursive
+		public Node search(Node node, int key) {
+			
+			while(node != null && key != node.data) {
+				
+				if(key < node.data) {
+					
+					node = node.left;
+				}
+				else {
+					
+					node = node.right;
+				}
+			}
+			
+			return node;
+		}
+		
+		public Node searchRecur(Node node, int key) {
+			
+			if(node != null && key != node.data) {
+				
+				if(key < node.data) {
+					
+					return searchRecur(node.left, key);
+				}
+				else {
+					
+					return searchRecur(node.right, key);
+				}
+			}
+			
+			return node;
+		}
+		
+		//5. minimum iterative and recursive
+		public Node minimum(Node node) {
+			
+			while(node.left != null) {
+				
+				node = node.left;
+			}
+			
+			return node;
+		}
+		
+		public Node minimumRecur(Node node) {
+			
+			if(node.left == null) {
+				
+				return node;
+			}
+			
+			return minimumRecur(node.left);
+		}
+		
+		//6. maximum iterative and recursive
+		public Node maximum(Node node) {
+			
+			while(node.right != null) {
+				
+				node = node.right;
+			}
+			
+			return node;
+		}
+		
+		public Node maximumRecur(Node node) {
+			
+			if(node.right == null) {
+				
+				return node;
+			}
+			
+			return maximumRecur(node.right);
 		}
 }
 
--- src/ds/binarysearchtree/BinarySearchTreeMain.java
+++ src/ds/binarysearchtree/BinarySearchTreeMain.java
@@ -5,8 +5,9 @@ public class BinarySearchTreeMain {
 	public static void main(String[] args) {
 
 		BinarySearchTree bst = new BinarySearchTree();
-		
-		//insert elements
+
+		// insert elements
+
 		bst.insert(bst.root, 30);
 		bst.insert(bst.root, 20);
 		bst.insert(bst.root, 40);
@@ -14,17 +15,45 @@ public class BinarySearchTreeMain {
 		bst.insert(bst.root, 25);
 		bst.insert(bst.root, 35);
 		bst.insert(bst.root, 50);
+
+		// insert elements
+		/*
+		 * bst.insertNode(bst.root, 30); bst.insertNode(bst.root, 20);
+		 * bst.insertNode(bst.root, 40); bst.insertNode(bst.root, 10);
+		 * bst.insertNode(bst.root, 25); bst.insertNode(bst.root, 35);
+		 * bst.insertNode(bst.root, 50);
+		 */
 		
-		//print elements
+		//search for a node
+		Node node = bst.search(bst.root, 25);
+		System.out.println("Found: " + node);
+		
+		node = bst.searchRecur(bst.root, 35);
+		System.out.println("Found: " + node);
+		
+		//find minimum and maximum in a tree
+		Node min = bst.minimum(bst.root);
+		System.out.println("Minimum: " + min);
+		
+		Node minVal = bst.minimumRecur(bst.root);
+		System.out.println("Minimum Val: " + minVal);
+		
+		Node max = bst.maximum(bst.root);
+		System.out.println("Maximum: " + max);
+		
+		Node maxVal = bst.maximumRecur(bst.root);
+		System.out.println("Maximum Val: " + maxVal);
+
+		// print elements
 		System.out.println("Preorder:");
 		bst.preorder(bst.root);
-		
+
 		System.out.println("Inorder:");
 		bst.inorder(bst.root);
-		
+
 		System.out.println("Postorder");
 		bst.postorder(bst.root);
-		
+
 	}
 
 }
